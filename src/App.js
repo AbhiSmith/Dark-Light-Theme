61232278bb245f2f2372f27a723e6a80026249cd
AbhiSmith <abhishekv4004@gmail.com>
2024-01-17 13:28:13 +0530

    final submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useLocalStorage from 'use-local-storage'
+
+import './index.css'
+
 
 function App() {
+
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' data-theme={theme}>
+      <div className='login'>
+          <h1> Login</h1>
+          <div className='container'>
+            <div className='top'>
+              <i className='fab fa-google'></i>
+              <i className='fab fa-facebook-square'></i>
+              <i className='fab fa-github'></i>
+              <i className='fab fa-apple'></i>
+              <i className='fab fa-linkedin'></i>              
+            </div>
+            <p className='divider'><span>or</span></p>
+            <form>
+              <label>Email</label>
+              <input type='email' placeholder='Enter your name'></input>
+              <label>Password</label>
+              <input type='password' placeholder='Enter your password'></input>
+              <div className='remember'>
+                <input type='checkbox' checked='checked'></input>
+                <p> Remember me</p>
+              </div>
+              <button>Login</button>
+            </form>
+            <div className='bottom'>
+              <p>Forgot your oassword</p>
+              <a href='#' >Reset your password</a>
+            </div>
+            <p className='create'>Create Account</p>            
+          </div>
+          <div className='theme-toggle'>
+              <h2>Light Theme</h2>
+              <i onClick={switchTheme} className='fas fa-toggle-on'></i>
+            </div>
+      </div>
     </div>
   );
 }
---
+++ src/README.md
@@ -0,0 +1,16 @@
+
+#  CSS Dark/Light/System Base Change With Local Storage | React js Dark Mode
+
+A brief description of what this project does and who it's for
+
+Start =>(npx create-react-app dark-theme)
+Hooks Togel dark & light theme=> (npm i use-local-storage)
+
+
+font awesome cdn [<script src="https://kit.fontawesome.com/bb7a83fd20.js" crossorigin="anonymous"></script>]
+
+
+## Authors
+
+- [@AbhiSmith](https://github.com/AbhiSmith)
+
